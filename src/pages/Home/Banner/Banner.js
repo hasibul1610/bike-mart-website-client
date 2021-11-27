b3caf36d0c9a440034fae5ada5880bdfb50349e8
hasibul1610 <bcse.hasibul@gmail.com>
2021-11-27 12:33:31 +0600

    updated

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import './Banner.css';
 import bannerBike from '../../../images/banner-bike.png';
+import RubberBand from 'react-reveal/RubberBand';
 
 const Banner = () => {
     return (
@@ -15,8 +16,14 @@ const Banner = () => {
                     </div>
                     <div className="col-md-6">
                         <div className="banner-details pt-5 mt-5">
-                            <h1>Life is Better on a Bike!!</h1>
-                            <h3>Let Us Help to Find Yours</h3>
+
+                            <RubberBand>
+                                <h1>Life is Better on a Bike!!</h1>
+                                <h3>Let Us Help to Find Yours</h3>
+
+                            </RubberBand>
+
+
                         </div>
                     </div>
                 </div>
